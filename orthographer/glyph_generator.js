009f32753fe318b233e5cd46156c3468eb4faed8
h4ck3rd4wg <adamlongname@gmail.com>
2017-04-05 09:19:37 -0700

    Going abroad

--- orthographer/glyph_generator.js
+++ orthographer/glyph_generator.js
@@ -12,16 +12,20 @@
 function generateGlyph(strokeNum, counter, unicode)
 {
 	var glyphPath = new opentype.Path();
+	var pathList = []; 
+	colors = ['red', 'blue', 'green', 'yellow', 'magenta', 'cyan', 'orange', 'purple'];
 
 	for(var i = 0; i < strokeNum; i++)
 	{
-		var start = RandomInsideBox();
-
 		var randomStroke = Math.floor(Math.random() * 3);
-		randomStroke = 2;
+		var strokeWidth = (Math.random() * 25) + 10;
 
-		addStroke(glyphPath, randomStroke, start, 25);
-		addContour(glyphPath);
+		var newPath = addStroke(randomStroke, strokeWidth);
+		var colorIndex = Math.floor(Math.random() * colors.length);
+		newPath.color = colors[colorIndex];
+		colors.splice(colorIndex, 1);
+		pathList.push(newPath);
+		addContour(newPath, glyphPath);
 	}
 
 	var glyph = new opentype.Glyph(
@@ -32,30 +36,34 @@ function generateGlyph(strokeNum, counter, unicode)
         advanceWidth: glyphScale,
         path: glyphPath
 	});
+	glyph.strokeData = pathList;
 	return glyph;
 }
 
-function addStroke(path, selection, point, width)
+function addStroke(selection, width)
 {
 	var stroke = [];
+	path = new opentype.Path();
+	start = RandomInsideBox();
+	end = RandomInsideBox();
 	switch(selection)
 	{
 		case 0:
-			stroke = generateLine(point, width);
+			stroke = generateLine(start, end, width);
 			break;
 		case 1:
-			stroke = generateQuadratic(point, width);
+			var control_point = RandomInsideBox();
+			stroke = generateQuadratic(start, end, control_point, width);
 			path.cp1Pos = stroke[5];
 			path.cp1Neg =  stroke[6];
-			/*path.start = stroke[7];
-			path.end = stroke[8];
-			path.cp = stroke[9];
-			path.startPerp=stroke[10];
-			path.endPerp=stroke[11];
-			path.perp = stroke[12];*/
+			path.cp1 = control_point;
 			break;
 		case 2:
-			stroke = generateCubic(point, width);
+			var control_point_1 = RandomInsideBox();
+			var control_point_2 = RandomInsideBox();
+			stroke = generateCubic(start, end, control_point_1, control_point_2, width);
+			path.cp1 = control_point_1;
+			path.cp2 = control_point_2;
 			path.cp1Pos = stroke[5];
 			path.cp1Neg =  stroke[6];
 			path.cp2Pos = stroke[7];
@@ -64,37 +72,42 @@ function addStroke(path, selection, point, width)
 		default:
 			console.log("Invalid stroke generation selection");
 	}
+	path.start = start;
+	path.end = end;
+	
 	path.command = stroke[0];
 	path.startPos = stroke[1];
 	path.startNeg = stroke[2];
 	path.endPos = stroke[3];
 	path.endNeg = stroke[4];
+
+	return path;
 }
 
 //Takes <opentype.js> Path object, a stroke object of up to three vectors containing 
 //the point and two control points for curves, and a code to describe the line
-function addContour(path)
+function addContour(path, glyphPath)
 {
-	path.moveTo(path.startNeg.x, path.startNeg.y);
-	path.lineTo(path.startPos.x, path.startPos.y);
+	glyphPath.moveTo(path.startNeg.x, path.startNeg.y);
+	glyphPath.lineTo(path.startPos.x, path.startPos.y);
 	switch(path.command)
 	{
 		case 'L':
-			path.lineTo(path.endPos.x, path.endPos.y);
-			path.lineTo(path.endNeg.x, path.endNeg.y);
-			path.lineTo(path.startNeg.x, path.startNeg.y);
+			glyphPath.lineTo(path.endPos.x, path.endPos.y);
+			glyphPath.lineTo(path.endNeg.x, path.endNeg.y);
+			glyphPath.lineTo(path.startNeg.x, path.startNeg.y);
 			break;
 		case 'Q':
-			path.quadTo(path.cp1Pos.x, path.cp1Pos.y, path.endPos.x, path.endPos.y);
-			path.lineTo(path.endNeg.x, path.endNeg.y);
-			path.quadTo(path.cp1Neg.x, path.cp1Neg.y, path.startNeg.x, path.startNeg.y);
+			glyphPath.quadTo(path.cp1Pos.x, path.cp1Pos.y, path.endPos.x, path.endPos.y);
+			glyphPath.lineTo(path.endNeg.x, path.endNeg.y);
+			glyphPath.quadTo(path.cp1Neg.x, path.cp1Neg.y, path.startNeg.x, path.startNeg.y);
 			break;
 		case 'C':
-			path.curveTo(path.cp1Pos.x, path.cp1Pos.y, 
+			glyphPath.curveTo(path.cp1Pos.x, path.cp1Pos.y, 
 								path.cp2Pos.x, path.cp2Pos.y,
 									path.endPos.x, path.endPos.y);
-			path.lineTo(path.endNeg.x, path.endNeg.y);
-			path.curveTo(path.cp2Neg.x, path.cp2Neg.y, 
+			glyphPath.lineTo(path.endNeg.x, path.endNeg.y);
+			glyphPath.curveTo(path.cp2Neg.x, path.cp2Neg.y, 
 								path.cp1Neg.x, path.cp1Neg.y,
 									path.startNeg.x, path.startNeg.y);			
 			break;
--- orthographer/primitive_strokes.js
+++ orthographer/primitive_strokes.js
@@ -1,14 +1,13 @@
 //Returns two points and a command 'L'
-function generateLine(point, width)
+function generateLine(start, end, width)
 {
-	var end = RandomInsideBox();
 	var offset = end.clone();
-	offset.subtract(point);
+	offset.subtract(start);
 	var perpLine = perpindicularLine(offset, true).normalize();
 
-	var spos = point.clone();
+	var spos = start.clone();
 	spos.add(new Victor(perpLine.x * width, perpLine.y * width));
-	var sneg = point.clone();
+	var sneg = start.clone();
 	sneg.add(new Victor(perpLine.x * -width, perpLine.y * -width));
 
 	var pos = end.clone();
@@ -34,20 +33,17 @@ function perpindicularLine(vector, left_handed)
 }
 
 //Returns four points and a command 'Q'
-function generateQuadratic(point, width)
-{
-	var end = RandomInsideBox();
-	var control_point = RandomInsideBox();
-	
-	var perpStart = perpindicularQuadratic([point, control_point, end], 0, true).normalize();
+function generateQuadratic(start, end, control_point, width)
+{	
+	var perpStart = perpindicularQuadratic([start, control_point, end], 0, true).normalize();
 	var perpCp = perpStart.clone();
 
-	var spos = point.clone();
+	var spos = start.clone();
 	spos.add(new Victor(perpStart.x * width, perpStart.y * width));
-	var sneg = point.clone();
+	var sneg = start.clone();
 	sneg.add(new Victor(perpStart.x * -width, perpStart.y * -width));
 
-	var perpEnd = perpindicularQuadratic([point, control_point, end], 1, true).normalize();
+	var perpEnd = perpindicularQuadratic([start, control_point, end], 1, true).normalize();
 	perpCp.add(perpEnd);
 
 	var pos = end.clone();
@@ -61,7 +57,7 @@ function generateQuadratic(point, width)
 	var cneg = control_point.clone();
 	cneg.add(new Victor(perpCp.x * -width * 2, perpCp.y * -width * 2));
 
-	return ['Q', spos, sneg, pos, neg, cpos, cneg, point, end, control_point, perpStart, perpEnd, perpCp];	
+	return ['Q', spos, sneg, pos, neg, cpos, cneg, perpStart, perpEnd, perpCp];	
 }
 
 //Takes a list of three points (start, control, end)
@@ -88,14 +84,10 @@ function quadraticDerivative(quad, t)
 }
 
 //Returns six points and a command 'C'
-function generateCubic(point, width)
+function generateCubic(start, end, control_point_1, control_point_2, width)
 {
-	var end = RandomInsideBox();
-	var control_point_1 = RandomInsideBox();
-	var control_point_2 = RandomInsideBox();
-
-	var spos = point.clone();
-	var sneg = point.clone();
+	var spos = start.clone();
+	var sneg = start.clone();
 	
 	var pos = end.clone();
 	var neg = end.clone();
@@ -106,13 +98,13 @@ function generateCubic(point, width)
 	var c2neg = control_point_2.clone();
 
 	var startPerp = 
-	perpindicularCubic([point, control_point_1, control_point_2, end], 0, true).normalize();
+	perpindicularCubic([start, control_point_1, control_point_2, end], 0, true).normalize();
 
 	spos.add(new Victor(startPerp.x * width, startPerp.y * width));
 	sneg.add(new Victor(startPerp.x * -width, startPerp.y * -width));
 
 	var endPerp = 
-	perpindicularCubic([point, control_point_1, control_point_2, end], 1, true).normalize();
+	perpindicularCubic([start, control_point_1, control_point_2, end], 1, true).normalize();
 	
 	pos.add(new Victor(endPerp.x * width, endPerp.y * width));	
 	neg.add(new Victor(endPerp.x * -width, endPerp.y * -width));
